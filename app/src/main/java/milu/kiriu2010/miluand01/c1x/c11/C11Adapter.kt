351af1364831f7d1c730742cebd6ccae3e2c332e
milukiriu2010 <milu.kiriu2010@gmail.com>
2020-06-13 11:30:49 +0900

    move item by long tap

--- app/src/main/java/milu/kiriu2010/miluand01/c1x/c11/C11Adapter.kt
+++ app/src/main/java/milu/kiriu2010/miluand01/c1x/c11/C11Adapter.kt
@@ -1,12 +1,16 @@
 package milu.kiriu2010.miluand01.c1x.c11
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.adapter_c11.view.*
 import milu.kiriu2010.entity.team0.Team0Abs
 import milu.kiriu2010.miluand01.R
 
@@ -19,6 +23,7 @@ import milu.kiriu2010.miluand01.R
 // -----------------------------------
 class C11Adapter(
     context: Context,
+    private val onC11StartDrag: OnC11StartDrag,
     // チーム一覧
     private val teamLst: MutableList<Team0Abs> = mutableListOf(),
     // チーム情報をクリック時に呼び出されるコールバック
@@ -27,16 +32,36 @@ class C11Adapter(
 
     private val inflater = LayoutInflater.from(context)
 
+    interface OnC11StartDrag {
+        fun startDragging(viewHolder: RecyclerView.ViewHolder)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): C11ViewHolder {
         val view = inflater.inflate(R.layout.adapter_c11, parent, false)
         val viewHolder = C11ViewHolder(view)
 
         // チーム情報をクリックしたときに呼び出されるコールバック
+        // --------------------------------------------------
+        // ドラッグ＆ドロップできるようにすると、呼び出されない
         view.setOnClickListener {
             val pos = viewHolder.adapterPosition
             val team = teamLst[pos]
             onTeamClicked(team)
         }
+
+        // タップすると、ドラッグ＆ドロップ可能なことをすぐ知らせるための状態に、即変化する
+        // これを入れると、画面領域を超えるスクロールができない
+        /*
+        viewHolder.itemView.hvC11.setOnTouchListener { v, event ->
+
+            if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+                onC11StartDrag.startDragging(viewHolder)
+            }
+
+            return@setOnTouchListener true
+        }
+         */
 
         return viewHolder
     }
@@ -50,8 +75,23 @@ class C11Adapter(
         holder.tvC11Name.text = team.name
     }
 
+    // アイテムを移動する
+    fun moveItem(from: Int, to: Int) {
+        val team = teamLst[from]
+        teamLst.removeAt(from)
+        teamLst.add(to,team)
+        notifyDataSetChanged()
+    }
+
+    // アイテム削除
+    fun deleteItem(pos: Int) {
+        teamLst.removeAt(pos)
+        notifyItemRemoved(pos)
+    }
+
     // チーム情報を表示するビューホルダー
     class C11ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val hvC11 = view.findViewById<ConstraintLayout>(R.id.hvC11)
         val ivC11Logo = view.findViewById<ImageView>(R.id.ivC11Logo)
         val tvC11Name = view.findViewById<TextView>(R.id.tvC11Name)
     }
--- app/src/main/java/milu/kiriu2010/miluand01/c1x/c11/C11Fragment.kt
+++ app/src/main/java/milu/kiriu2010/miluand01/c1x/c11/C11Fragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import milu.kiriu2010.entity.team0.Team0Abs
@@ -17,10 +19,72 @@ import milu.kiriu2010.miluand01.R
 // -----------------------------------
 // チーム一覧を表示するフラグメント
 // -----------------------------------
-class C11Fragment : Fragment() {
+class C11Fragment : Fragment(),
+    C11Adapter.OnC11StartDrag {
+
 
     // チーム一覧を表示するリサイクラビュー
     private lateinit var rvC11: RecyclerView
+
+    // ロングタッチで移動できるようにするためのおまじない
+    // https://medium.com/@yfujiki/drag-and-reorder-recyclerview-items-in-a-user-friendly-manner-1282335141e9
+    private val itemTouchHelper by lazy {
+        // 1. Note that I am specifying all 4 directions.
+        //    Specifying START and END also allows
+        //    more organic dragging than just specifying UP and DOWN.
+        val simpleItemTouchCallback =
+            object: ItemTouchHelper.SimpleCallback(UP or DOWN or START or END,
+                0) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    val adapter = recyclerView.adapter as C11Adapter
+                    val from = viewHolder.adapterPosition
+                    val to = target.adapterPosition
+                    // 2. Update the backing model. Custom implementation in
+                    //    MainRecyclerViewAdapter. You need to implement
+                    //    reordering of the backing model inside the method.
+                    adapter.moveItem(from, to)
+                    // 3. Tell adapter to render the model update.
+                    adapter.notifyItemMoved(from, to)
+
+                    return true
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    // 4. Code block for horizontal swipe.
+                    //    ItemTouchHelper handles horizontal swipe as well, but
+                    //    it is not relevant with reordering. Ignoring here.
+                    val adapter = rvC11.adapter as C11Adapter
+                    adapter.deleteItem(viewHolder.adapterPosition)
+                }
+
+                // onSelectedChanged と clearView で
+                // ロングタッチでドラッグ＆ドロップができることを知らせるおまじない
+
+                // 1. This callback is called when a ViewHolder is selected.
+                //    We highlight the ViewHolder here.
+                override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?,
+                                               actionState: Int) {
+                    super.onSelectedChanged(viewHolder, actionState)
+
+                    if (actionState == ACTION_STATE_DRAG) {
+                        viewHolder?.itemView?.alpha = 0.5f
+                    }
+                }
+                // 2. This callback is called when the ViewHolder is
+                //    unselected (dropped). We unhighlight the ViewHolder here.
+                override fun clearView(recyclerView: RecyclerView,
+                                       viewHolder: RecyclerView.ViewHolder) {
+                    super.clearView(recyclerView, viewHolder)
+                    viewHolder.itemView.alpha = 1.0f
+                }
+            }
+
+        ItemTouchHelper(simpleItemTouchCallback)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +107,7 @@ class C11Fragment : Fragment() {
 
         // チーム情報を表示するためのアダプタ
         context?.let {
-            val adapter = C11Adapter(it, createData()) {
+            val adapter = C11Adapter(it, this, createData()) {
                 // チーム情報をクリック時に呼び出されるコールバック
 
                 val toast = Toast.makeText(context,it.name,Toast.LENGTH_SHORT)
@@ -51,6 +115,9 @@ class C11Fragment : Fragment() {
             }
             rvC11.adapter = adapter
         }
+
+        // ロングタッチによるアイテム移動をするために、補助とリサイクラビューを結びつける
+        itemTouchHelper.attachToRecyclerView(rvC11)
 
         return view
     }
@@ -70,6 +137,11 @@ class C11Fragment : Fragment() {
         return teamLst
     }
 
+    // ドラッグ＆ドロップ
+    override fun startDragging(viewHolder: RecyclerView.ViewHolder) {
+        itemTouchHelper.startDrag(viewHolder)
+    }
+
     companion object {
         @JvmStatic
         fun newInstance() =
@@ -78,4 +150,5 @@ class C11Fragment : Fragment() {
                 }
             }
     }
+
 }
